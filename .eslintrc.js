efb820b3a9e4a4d6acfd83d1537f1ba17e73f436
dan0x09 <danielkoenig95@web.de>
2018-06-22 04:11:55 +0200

    Add ESLint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,22 @@ module.exports = {
   },
   rules: {
 	'import/no-unresolved': 0,
-	'import/extensions': 0
+	'import/extensions': 0,
+	'no-param-reassign': 0,
+	'radix': 0,
+	'vars-on-top': 0,
+	'no-shadow': 0,
+	'no-use-before-define': 0,
+	'camelcase': 0,
+	'comma-dangle': 0,
+	'no-plusplus': 0,
+	'no-var': 0,
+	'object-shorthand': 0,
+	'prefer-arrow-callback': 0,
+	'prefer-rest-params': 0,
+	'prefer-spread': 0,
+	'func-names': 0,
+	'no-underscore-dangle': 0
 	
   },
   overrides: [
